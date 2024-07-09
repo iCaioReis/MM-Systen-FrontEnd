880a30f0767d8e24da00a558525766202f663ab4
iCaioReis <caioreis3186@gmail.com>
2024-07-09 09:50:19 -0300

    I deleted unnecessary files, created global styles and themes

---
+++ src/styles/theme.js
@@ -0,0 +1,16 @@
+export default {
+    COLORS: {
+    LIGHT_100: "#FFFFFF",
+    LIGHT_200: "#E3EFDA",
+    LIGHT_300: "#D6E7B4",
+    LIGHT_400: "#9ABB92",
+
+    THEME_600: "#335313",
+    THEME_700: "#0D3A0C",
+    THEME_900: "#0A2006",
+
+    BLACK: "#000000",
+
+    RED: "#D21010",
+    }
+  };
